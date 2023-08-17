3bb1563aa190066054b24721dae83dfcca916c02
Fram <agreen90@yandex.ru>
2023-08-17 23:47:17 +0400

    ref

---
+++ aTES.TaskTracker/Kafka/Models/TaskChangedStreamingModel.cs
@@ -0,0 +1,29 @@
+﻿using Task = aTES.TaskTracker.Domain.Task;
+
+namespace aTES.TaskTracker.Kafka.Models;
+
+public class TaskChangedStreamingModel
+{
+    public Guid AssigneePublicId { get; set; }
+    public Guid TaskPublicId { get; set; }
+    public string Description { get; set; }
+    public string Status { get; set; }
+    public decimal ChargePrice { get; set; }
+    public decimal PaymentPrice { get; set; }
+    public DateTimeOffset? CompletedAt { get; set; }
+
+    
+    public static TaskChangedStreamingModel FromDomain(Task task)
+    {
+        return new TaskChangedStreamingModel()
+        {
+            AssigneePublicId = task.PopugPublicId,
+            TaskPublicId = task.PublicId,
+            Description = task.Description,
+            Status = task.Status.ToString(),
+            ChargePrice = task.ChargePrice,
+            PaymentPrice = task.PaymentPrice,
+            CompletedAt = task.CompletedAt
+        };
+    }
+}
---
+++ aTES.TaskTracker/Kafka/Models/TaskCreatedBusinessModel.cs
@@ -0,0 +1,24 @@
+﻿using Task = aTES.TaskTracker.Domain.Task;
+
+namespace aTES.TaskTracker.Kafka.Models;
+
+public class TaskCreatedBusinessModel
+{
+    public Guid AssigneePublicId { get; set; }
+    public Guid TaskPublicId { get; set; }
+    public string Description { get; set; }
+    public decimal ChargePrice { get; set; }
+    public decimal PaymentPrice { get; set; }
+
+    public static TaskCreatedBusinessModel FromDomain(Task task)
+    {
+        return new TaskCreatedBusinessModel()
+        {
+            AssigneePublicId = task.PopugPublicId,
+            TaskPublicId = task.PublicId,
+            Description = task.Description,
+            ChargePrice = task.ChargePrice,
+            PaymentPrice = task.PaymentPrice,
+        };
+    }
+}
